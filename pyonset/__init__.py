5e53470cb09424ff52e84510833fc4fef062e072
Jan Gieseler <jan.gieseler@utu.fi>
2025-02-06 12:04:48 +0200

    restructure package to follow updated style

--- pyonset/__init__.py
+++ pyonset/__init__.py
@@ -1,10 +1,7 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    pass  # package is not installed
+from .version import version as __version__
+
+# __all__ = []  # https://stackoverflow.com/questions/44834/what-does-all-mean-in-python
 
 import datetime
 import glob
