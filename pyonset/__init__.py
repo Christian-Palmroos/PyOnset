c92ad69e43a79b53853728cfeb3532defe61496f
Christian-Palmroos <chospa@utu.fi>
2025-08-11 16:08:38 +0300

    Updated the @Updated date.

--- pyonset/__init__.py
+++ pyonset/__init__.py
@@ -8,7 +8,7 @@ A library that holds the Onset class for PyOnset.
 
 @Author: Christian Palmroos <chospa@utu.fi>
 
-@Updated: 2025-06-02
+@Updated: 2025-08-11
 
 Known problems/bugs:
     > Does not work with SolO/STEP due to electron and proton channels not defined in all_channels() -method
