7305f8f373563e9e4773df718c472aa2e6cbd64b
Jan Gieseler <39489154+jgieseler@users.noreply.github.com>
2025-11-28 10:37:12 +0200

    Uncomment version import in __init__.py

--- pyonset/__init__.py
+++ pyonset/__init__.py
@@ -1,5 +1,5 @@
 
-#from .version import version as __version__
+from .version import version as __version__
 
 # __all__ = []  # defines which functions, variables etc. will be loaded when running "from pyonset import *"
 
